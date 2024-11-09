58c49b13a3386dab3ce59acecf99dd8ce2a2749a
DaoNgocAnhKhoi <daongocanhkhoi@gmail.com>
2024-11-09 10:25:58 +0700

    merged

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { useTheme } from "react-native-paper";
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
