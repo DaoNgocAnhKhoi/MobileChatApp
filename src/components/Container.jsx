0785c66986113261db21f6ca057f0c756884b5fc
Dam Thanh Lam <ldamthanh62@gmail.com>
2024-11-09 10:17:17 +0700

    fix /components/Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,25 +3,26 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { useTheme } from "react-native-paper";
 
-const Container = ({ children, style }) => {
-    const theme = useTheme();
-    const { colors } = theme;
-  return (
-    <View style={[styles.container, { backgroundColor: colors.background }, style]}>
-        {children}
-    </View>
-  );
-};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: "100%",
-    paddingTop:40,
+    paddingTop: 40,
     paddingBottom: 20,
     paddingHorizontal: 16,
     justifyContent: "center",
   },
 });
+const Container = ({ children, style }) => {
+  const theme = useTheme();
+  const { colors } = theme;
+  return (
+    <View style={[styles.container, { backgroundColor: colors.background }, style]}>
+      {children}
+    </View>
+  );
+};
 
 export default Container;
